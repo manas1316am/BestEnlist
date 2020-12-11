0157ef05fdae472379fbf0dfdb386c8b48574642
manas1316am <ad1316a@gmail.com>
2020-12-11 17:37:22 +0530

    Initial Commit

---
+++ fiveteenth_day/app.js
@@ -0,0 +1,18 @@
+function formatMoney(amount, decimalCount = 2, decimal = ".", thousands = ",") {
+    try {
+      decimalCount = Math.abs(decimalCount);
+      decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+  
+      const negativeSign = amount < 0 ? "-" : "";
+  
+      let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+      let j = (i.length > 3) ? i.length % 3 : 0;
+  
+      return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+    } catch (e) {
+      console.log(e)
+    }
+  };
+  document.getElementById("b").addEventListener("click", event => {
+    document.getElementById("x").innerText = "Result was: " + formatMoney(document.getElementById("d").value);
+  });
